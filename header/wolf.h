c674ad1a2b7ed8c5eb4d56651f9fb66d7fecef13
FenrirAetherius <blueyesmax69@gmail.com>
2020-02-03 16:55:46 +0100

    some advencment in my struct and ft_checkmap

--- header/wolf.h
+++ header/wolf.h
@@ -5,17 +5,36 @@
 
 typedef struct		s_wolf
 {
-	t_map			*t_map;
+	t_map			**map;
 	t_sprite		*sprite;
+	char 			*file;
+	char 			*line;
+	int				fd;
+	char 			ret;
+	char 			ok;
+	short			error;
 }					t_wolf;
 
 typedef struct		s_sprite
 {
+	char 			*no;
+	char 			*so;
+	char 			*we;
+	char 			*ea;
+	char 			*s;
+	char 			*f;
+	char 			*c;
 }					t_sprite;
 
-typedef struct	s_map
+typedef struct	s_map_init
 {
 	char			*content;
 	struct s_list	*next;
+}				t_map_init;
+
+typedef struct	s_map
+{
+	char 		**map;
 }				t_map;
+
 #endif
