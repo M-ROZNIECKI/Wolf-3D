293b2da22753e7147a72411412ca0a0517e48ca9
FenrirAetherius <blueyesmax69@gmail.com>
2020-02-03 16:55:46 +0100

    some advencment in my struct and ft_checkmap

--- checkmap.c
+++ checkmap.c
@@ -13,38 +13,31 @@
 
 #include "header/wolf.h"
 
-int	checkmap(char *filemap)
+int	ft_checkmap(t_wolf *wolf)
 {
-	char *line;
-	int fd;
-	int ret;
-	char ok;
-
-	fd = open(filemap);
-	while ((ret = getnextline(fd, &line)) != 0)
+	while ((wolf->ret = getnextline(wolf->fd, &wolf->line)) != 0)
 	{
-		if (line[0] == '1' && ok == 0xFF)
+		if (wolf->line[0] == '1' && wolf->ok == 0xFF)
 			ft_fillmap();
-		else if (line[0] == 'N' && line[1] == 'O')//	0b00000010
+		else if (wolf->line[0] == 'N' && wolf->line[1] == 'O')//	0b00000010
 			ft_fillsprite();
-		else if (line[0] == 'S' && line[1] == 'O')//	0b00000100
+		else if (wolf->line[0] == 'S' && wolf->line[1] == 'O')//	0b00000100
 			ft_fillsprite();
-		else if (line[0] == 'W' && line[1] == 'E')//	0b00001000
+		else if (wolf->line[0] == 'W' && wolf->line[1] == 'E')//	0b00001000
 			ft_fillsprite();
-		else if (line[0] == 'E' && line[1] == 'A')//	0b00010000
+		else if (wolf->line[0] == 'E' && wolf->line[1] == 'A')//	0b00010000
 			ft_fillsprite();
-		else if (line[0] == 'S' && line[1] != 'O')//	0b00100000
+		else if (wolf->line[0] == 'S' && wolf->line[1] != 'O')//	0b00100000
 			ft_fillsprite();
-		else if (line[0] == 'F')//						0b01000000
+		else if (wolf->line[0] == 'F')//						0b01000000
 			ft_fillsprite();
-		else if (line[0] == 'C')//						0b10000000
+		else if (wolf->line[0] == 'C')//						0b10000000
 			ft_fillsprite();
-		else if (line[0] == 'R')//						0b00000001
+		else if (wolf->line[0] == 'R')//						0b00000001
 			ft_fillres();
-		else if (line[0] != '\0' || ret == -1)
-		{
-			free(line);
-			ft_error(ret);//							-1 error durring processing of the map file \\ 1 map file incorectly formated
-		}
+		else if (wolf->line[0] != '\0' || wolf->ret == -1)
+			ft_error(wolf->ret, line);//							-1 error durring processing of the map file \\ 1 map file incorectly formated
+		free(line);
 	}
+	return (wolf->error);
 }
