e3b2730c959e9f3e47b93d51ac3919163460e1ac
FenrirAetherius <blueyesmax69@gmail.com>
2020-02-28 14:17:44 +0100

    test for parse ended, for the time being i'm setting hard in a define the maximum resolution of the screen at the school, need to find a way to get that info

--- main-test.c
+++ main-test.c
@@ -6,7 +6,7 @@
 /*   By: mrozniec <mrozniec@student.le-101.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/27 17:45:00 by mrozniec          #+#    #+#             */
-/*   Updated: 2020/02/27 18:59:45 by mrozniec         ###   ########lyon.fr   */
+/*   Updated: 2020/02/27 19:10:02 by mrozniec         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,21 +29,20 @@
 
 int main(int argc, char const *argv[])
 {
-//	void *fistyland;
-//	void *my_mlx;
+	void *fistyland;
+	void *my_mlx;
 	t_wolf *wolf;
 
 	if (!(wolf = malloc(sizeof(t_wolf))))
 		ft_error(-2, __LINE__, __FILE__, __FUNCTION__);
-	if (argc == 2)
-	{
+	if (argc == 2) {
 		wolf->fd = open(argv[1], O_RDONLY);
 		wolf->ok = 0;
 		ft_init_map(wolf);
+		printf("everything is ok");
+		my_mlx = mlx_init();
+		fistyland = mlx_new_window(my_mlx, wolf->win.res_x, wolf->win.res_y, "E1M1");
+		mlx_loop(my_mlx);
 	}
-	printf("everything is ok");
-/*	my_mlx = mlx_init();
-	fistyland = mlx_new_window(my_mlx, wolf->win.res_x, wolf->win.res_y, "hello");
-	mlx_loop(my_mlx);*/
 	return (0);
 }
