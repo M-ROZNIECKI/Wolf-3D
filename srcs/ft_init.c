4137bb0802e638143c189c5dc59616eb8d8f50d8
fenriraetherius <blueyesmax69@gmail.com>
2020-03-12 00:55:28 +0100

    starting graphic init part

---
+++ srcs/ft_init.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_init.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fenrir <fenrir@student.le-101.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/11 21:35:45 by fenrir            #+#    #+#             */
+/*   Updated: 2020/03/11 21:35:53 by fenrir           ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../header/wolf.h"
+
+void	ft_init_w(t_win *win)
+{
+	win->my_mlx = mlx_init();
+	win->win = mlx_new_windows(win->my_mlx, win->res_x, win->res_y, "NOP");
+}
+
+void	ft_init(t_wolf *wolf)
+{
+	ft_init_map(wolf);
+	ft_init_w(&wolf->win);
+}
