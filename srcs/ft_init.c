f81da1932f38000820c020891aaa91be4c300c53
FenrirAetherius <blueyesmax69@gmail.com>
2020-05-05 12:49:02 +0200

    update error message and correction merge problem

--- srcs/ft_init.c
+++ srcs/ft_init.c
@@ -5,30 +5,22 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: fenrir <fenrir@student.le-101.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/03/11 21:35:45 by fenrir            #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2020/03/12 03:05:42 by fenrir           ###   ########lyon.fr   */
-=======
-/*   Updated: 2020/05/02 10:35:08 by fenrir           ###   ########lyon.fr   */
->>>>>>> test_map
+/*   Created: 2020/05/05 12:42:38 by fenrir            #+#    #+#             */
+/*   Updated: 2020/05/05 12:42:38 by fenrir           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/wolf.h"
 
-static void	ft_init_w(t_win *win)
+void	ft_init_w(t_win *win)
 {
 	win->my_mlx = mlx_init();
-	win->win = mlx_new_windows(win->my_mlx, win->res_x, win->res_y, win->name);
+	win->win = mlx_new_windows(win->my_mlx, win->res_x, win->res_y, "NOP");
 }
 
-void		ft_init(t_wolf *wolf)
+void	ft_init(t_wolf *wolf)
 {
 	ft_init_map(wolf);
-<<<<<<< HEAD
-	ft_init_w(&wolf->win);
-	ft_init_tex(wolf);
-=======
 	//ft_init_w(&wolf->win);
->>>>>>> test_map
+}it_tex(wolf);
 }
