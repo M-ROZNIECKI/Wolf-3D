44ebadab2b23d6aee0561658e1aed2a6441dbfc3
FenrirAetherius <blueyesmax69@gmail.com>
2020-05-05 11:06:34 +0200

    map.cub parsing validated and work with space as void case in the map

--- srcs/ft_utils_map.c
+++ srcs/ft_utils_map.c
@@ -6,35 +6,34 @@
 /*   By: mrozniec <mrozniec@student.le-101.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/27 17:33:51 by mrozniec          #+#    #+#             */
-/*   Updated: 2020/02/27 17:33:51 by mrozniec         ###   ########lyon.fr   */
+/*   Updated: 2020/05/05 10:43:11 by fenrir           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/wolf.h"
 
-void	ft_start_pos(t_map *map)
+void	ft_test_pos(t_map *map)
 {
-	map->temp_y = 0;
-	map->temp_x = 0;
-	while (map->temp_y < map->map_y &&
-		   map->map[map->temp_y][map->temp_x] != 'N' &&
-		   map->map[map->temp_y][map->temp_x] != 'S' &&
-		   map->map[map->temp_y][map->temp_x] != 'E' &&
-		   map->map[map->temp_y][map->temp_x] != 'W')
+	static unsigned int i = 0;
+	if (map->map[map->temp_y][map->temp_x] == 'N' ||
+		map->map[map->temp_y][map->temp_x] == 'S' ||
+		map->map[map->temp_y][map->temp_x] == 'E' ||
+		map->map[map->temp_y][map->temp_x] == 'W')
 	{
-		map->temp_x = 0;
-		while (map->map[map->temp_y][map->temp_x] &&
-			   map->map[map->temp_y][map->temp_x] != 'N' &&
-			   map->map[map->temp_y][map->temp_x] != 'S' &&
-			   map->map[map->temp_y][map->temp_x] != 'E' &&
-			   map->map[map->temp_y][map->temp_x] != 'W')
-			map->temp_x++;
-		if (map->map[map->temp_y][map->temp_x] != 'N' &&
-			map->map[map->temp_y][map->temp_x] != 'S' &&
-			map->map[map->temp_y][map->temp_x] != 'E' &&
-			map->map[map->temp_y][map->temp_x] != 'W')
-			map->temp_y++;
+		map->start_x = map->temp_x;
+		map->start_y = map->temp_y;
+		i++;
 	}
-	map->start_x = map->temp_x;
-	map->start_y = map->temp_y;
+	if (map->temp_y == 0 || map->temp_x == 0 ||
+		map->temp_y == map->map_y - 1 || i > 1 ||
+		map->map[map->temp_y - 1][map->temp_x - 1] == ' ' ||
+		map->map[map->temp_y - 1][map->temp_x] == ' ' ||
+		map->map[map->temp_y - 1][map->temp_x + 1] == ' ' ||
+		map->map[map->temp_y + 1][map->temp_x - 1] == ' ' ||
+		map->map[map->temp_y + 1][map->temp_x] == ' ' ||
+		map->map[map->temp_y + 1][map->temp_x + 1] == ' ' ||
+		map->map[map->temp_y][map->temp_x - 1] == ' ' ||
+		map->map[map->temp_y][map->temp_x + 1] == ' ' ||
+		map->map[map->temp_y][map->temp_x + 1] == '\0')
+		ft_error(1, __LINE__, __FILE__, __FUNCTION__);
 }
